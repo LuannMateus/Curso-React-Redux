6ac582c8f618338ea45daaa40cf3c64912deec88
LuannMateus <luanmateuslima34@gmail.com>
2020-12-18 18:19:27 -0300

    Uploading Controlled Component class

--- fundamentos-react/src/components/forms/Input.jsx
+++ fundamentos-react/src/components/forms/Input.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Input.css';
 
-const form = props => {
+const Form = props => {
+    const [value, setState] = useState('Initial');
+
+    function changeValue(e) {
+        setState(e.target.value)
+    }
+
     return (
         <div>
-            Input
+            <h1>{value ? value : 'Initial'}</h1 >
+            <div className='form'>
+                {/*Componente Controlado*/}
+                <h2>Componente Controlado</h2>
+                <input type="text" value={value} onChange={changeValue} />
+
+                {/* Componente de apenas leitura */}
+                <h2>Componente de apenas leitura</h2>
+                <input type="text" value={value} readOnly />
+
+                {/*Componente Não Controlado*/}
+                <h2>Componente Não Controlado</h2>
+                <input type="text" value={undefined} />
+            </div>
         </div>
     )
 }
 
-export default form;
+export default Form;
