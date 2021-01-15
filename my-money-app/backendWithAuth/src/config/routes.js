28162b342db48e9e775d8c3ab3fcb08e602c0aa3
LuannMateus <luanmateuslima34@gmail.com>
2021-01-15 15:00:08 -0300

    Add Backend with Auth

---
+++ my-money-app/backendWithAuth/src/config/routes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const auth = require('./auth.js');
+
+
+module.exports = function (server) {
+    /*
+        * Rotas protegidas por Token JWT
+    */
+
+    const protectedApi = express.Router();
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
+
+    const BillingCycle = require('../../src/api/billingCycle/billingCycleService.js');
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    /*
+        * Rotas abertas
+    */
+    const openApi = express.Router()
+    server.use('oapi', openApi)
+
+    const AuthService = require('../api/user/authService.js')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/singup', AuthService.singup)
+    openApi.post('/validateToken', AuthService.validateToken)
+} 
